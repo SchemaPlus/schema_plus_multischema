e2e4a8a5e98d104789f5fce4ad8ee48169442e0e
ronen barzel <ronen@barzel.org>
2015-08-28 11:24:43 +0100

    Simplify query code.
    No need to go through arel and bindings since it’s hardwired SQL; use higher-level connection.exec_query method for simpler API

--- lib/schema_plus_multischema/middleware.rb
+++ lib/schema_plus_multischema/middleware.rb
@@ -7,26 +7,14 @@ module SchemaPlusMultischema
         module Tables
 
           def implement(env)
-            env.tables += select_tablenames_with_schemas env.connection, <<-SQL
-            SELECT schemaname, tablename
-            FROM pg_tables
-            WHERE schemaname = ANY(current_schemas(false))
+            query = <<-SQL
+              SELECT schemaname, tablename
+              FROM pg_tables
+              WHERE schemaname = ANY(current_schemas(false))
             SQL
-          end
-
-          private
-
-          def select_tablenames_with_schemas(connection, arel)
-            arel, binds = connection.send :binds_from_relation, arel, []
-            sql = connection.to_sql(arel, binds)
-            connection.send :execute_and_clear, sql, 'SCHEMA', binds do |result|
-              if result.nfields > 0
-                rows = result.column_values(0).count
-                (0..(rows - 1)).map{ |i| "#{result.column_values(0)[i]}.#{result.column_values(1)[i]}" }
-              else
-                []
-              end
-            end
+            env.tables += env.connection.exec_query(query, 'SCHEMA').map { |row|
+              "#{row['schemaname']}.#{row['tablename']}"
+            }
           end
 
         end
