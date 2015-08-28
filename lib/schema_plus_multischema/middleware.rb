651d4b58e1d67de7cc24bdff136c422b643ddfb4
ronen barzel <ronen@barzel.org>
2015-08-28 13:12:55 +0100

    don’t add prefix when using default schema path

--- lib/schema_plus_multischema/middleware.rb
+++ lib/schema_plus_multischema/middleware.rb
@@ -6,14 +6,21 @@ module SchemaPlusMultischema
       module Schema
         module Tables
 
+          DEFAULT_SCHEMA_SEARCH_PATH = %q{"$user",public}
+
           def implement(env)
+            use_prefix = (env.connection.schema_search_path != DEFAULT_SCHEMA_SEARCH_PATH)
             query = <<-SQL
               SELECT schemaname, tablename
               FROM pg_tables
               WHERE schemaname = ANY(current_schemas(false))
             SQL
             env.tables += env.connection.exec_query(query, 'SCHEMA').map { |row|
-              "#{row['schemaname']}.#{row['tablename']}"
+              if use_prefix
+                "#{row['schemaname']}.#{row['tablename']}"
+              else
+                row['tablename']
+              end
             }
           end
 
