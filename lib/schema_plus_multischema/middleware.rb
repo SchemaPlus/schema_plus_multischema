2b6bc15b719a81cd8e10dc238139babbfd9a0646
ronen barzel <ronen@barzel.org>
2016-03-24 15:21:10 -0400

    more robust check for default search path, works with posgresql 9.5

--- lib/schema_plus_multischema/middleware.rb
+++ lib/schema_plus_multischema/middleware.rb
@@ -2,13 +2,13 @@ module SchemaPlusMultischema
   module Middleware
 
     module PostgreSQL
-      DEFAULT_SCHEMA_SEARCH_PATH = %q{"$user",public}
+      DEFAULT_SCHEMA_SEARCH_PATH = %r{"\$user",\s*public}
 
       module Schema
         module Tables
 
           def implement(env)
-            use_prefix = (env.connection.schema_search_path != DEFAULT_SCHEMA_SEARCH_PATH)
+            use_prefix = (env.connection.schema_search_path !~ DEFAULT_SCHEMA_SEARCH_PATH)
             query = <<-SQL
               SELECT schemaname, tablename
               FROM pg_tables
@@ -30,7 +30,7 @@ module SchemaPlusMultischema
         module Initial
 
           def after(env)
-            if (path = env.connection.schema_search_path) != DEFAULT_SCHEMA_SEARCH_PATH
+            if (path = env.connection.schema_search_path) !~ DEFAULT_SCHEMA_SEARCH_PATH
               path.split(',').each do |name|
                 env.initial << %Q{  connection.execute "CREATE SCHEMA IF NOT EXISTS #{name}"}
               end
